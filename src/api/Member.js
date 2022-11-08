7c12d7eb38036647d04f023085bbd854ff7bce6a
yangzhe <yancy@tdeado.com>
2022-11-08 22:26:40 +0800

    新增抽奖

--- src/api/Member.js
+++ src/api/Member.js
@@ -3,6 +3,38 @@ import {serviceUrl} from '@/utils/request'
 
 
 /**
+ * 用户注册
+ * @param queryParams 参数对象
+ * @param options ajax其他参数
+*/
+export function memberRegister (queryParams,options) {
+    return request(Object.assign({
+    url: serviceUrl+'/survey/api/register',
+    method: 'POST',
+    params:queryParams,
+    headers: {
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    }
+    },options))
+}
+
+/**
+ * 用户登录
+ * @param queryParams 参数对象
+ * @param options ajax其他参数
+*/
+export function memberLogin (queryParams,options) {
+    return request(Object.assign({
+    url: serviceUrl+'/survey/api/login',
+    method: 'POST',
+    params:queryParams,
+    headers: {
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    }
+    },options))
+}
+
+/**
  * 
  * @param queryParams 参数对象
  * @param options ajax其他参数
